833be1bd95a5972e671bb1ed876e2baf1f211dad
SagivDaniel+1-at-345037243928 <77956838+DanielSagiv@users.noreply.github.com>
2025-09-18 13:01:35 +0300

    yet1

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('THEONE SERVER TEST');
+  res.send('THEONE SERVER TEST 1');
 });
 
 app.get('/health', (req, res) => {
-  res.send('healthy');
+  res.send('healthy TEST');
 });
 
 // Start server
